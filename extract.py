3799fd7a425fe0dfe7b0d79d7ec8be6be99ced83
ahmed-menrva <ahmedotifi11112000@gmail.com>
2025-05-25 11:34:37 +0300

    add APIKEY

--- extract.py
+++ extract.py
@@ -4,7 +4,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 def extract_weather_data(city="Cairo"):
-    url = f"http://api.weatherapi.com/v1/current.json?key=YOUR_API_KEY&q={city}"
+    url = f"http://api.weatherapi.com/v1/current.json?key=da97b518669e4ee4ae5113321252505&q={city}"
     response = requests.get(url)
     response.raise_for_status()  # Raises error if not 200
     logging.info("✅ Weather data extracted.")
